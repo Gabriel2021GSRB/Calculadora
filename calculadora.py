34f63b3db9ff835589e2dc3f3352c3cab0573f24
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-02-18 13:10:20 -0300

    O usuario vai decidir qual operaão vai efetuar.

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+numero_1 = 0
+numero_2 = 0
+operacao = "..."
+operacao_1 = 'adicao'
+operacao_2 = 'subtracao'
+operacao_3 = 'multiplicacao'
+operacao_4 = 'divisao'
+
+# Aqui vamos dar para o usuario decidir qual operação ele deseja efetuar.
+
+operacao = str(input("digite a operação que você deseja efetuar"))
+if operacao_1 == '+':
+    print("vamos começar as operações de adição")
+elif operacao_2 == '-':
+    print("vamos começar as operações de subtração")
+elif operacao_3 == '*': 
+    print("vamos começar as operações de multiplicação")
+elif operacao_4 == '/':
+    print("vamos começar as operações de adição")
