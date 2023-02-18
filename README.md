b8e5092fa147f639cc17e8595372fa7776a71c0f
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-02-18 13:55:14 -0300

    Trabalhando para solucionar o erro

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Calculadora
 Aqui vamos começar um novo projeto onde vai ser desenvolvido uma cauculadora em Python.
+
+[] senha
+[] adição
+[] subtração
+[] multiplicação
+[] divisão
+[]
+[]
--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,36 @@
 numero_1 = 0
 numero_2 = 0
-operacao = "..."
-operacao_1 = 'adicao'
-operacao_2 = 'subtracao'
-operacao_3 = 'multiplicacao'
-operacao_4 = 'divisao'
-
+operacao = 0
+operacao1 = 1
+operacao2 = 2
+operacao3 = 3
+operacao4 = 4
 # Aqui vamos dar para o usuario decidir qual operação ele deseja efetuar.
-
-operacao = str(input("digite a operação que você deseja efetuar"))
-if operacao_1 == '+':
+operacao = int(input("Digite a operacao desejada (1 para soma, 2 para sub, 3 para mult, 4 para div): "))
+if operacao1 == 1:
     print("vamos começar as operações de adição")
-elif operacao_2 == '-':
+elif operacao2 == 2:
     print("vamos começar as operações de subtração")
-elif operacao_3 == '*': 
+elif operacao3 == 3: 
     print("vamos começar as operações de multiplicação")
-elif operacao_4 == '/':
-    print("vamos começar as operações de adição")
+elif operacao4 == 4:
+    print("vamos começar as operações de dividir")
+else: 
+    print("opção invalida")
+
+
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+
+if operacao == "soma":
+	resultado = int(numero1) + int(numero2)
+if operacao == "sub":
+	resultado = int(numero1) - int(numero2)
+if operacao == "mult":
+	resultado = int(numero1) * int(numero2)
+if operacao == "div":
+	resultado = int(numero1) / int(numero2)
+else:
+	resultado = "Operação não suportada"
+    
+print("O resultado da operação é: " + str(resultado))
