46143744be0d4586f0820bea95f72a918a3ec93e
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-03-20 16:48:51 -0300

    adição de laço de repetição

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Calculadora
 Aqui vamos começar um novo projeto onde vai ser desenvolvido uma cauculadora em Python.
+No sistema foi aderido o laço de repetição e estruturas condicionais.
 
 
 [x] adição
--- calculadora.py
+++ calculadora.py
@@ -3,36 +3,34 @@ numero_2 = 0
 operacao = 0
 resultado = ""
 
-print("*******************************************************************")
+while operacao != "s": # Aqui o usuario vai decidir se deseja faser outra operação ou não.
+
 # Aqui vamos dar para o usuario decidir qual operação ele deseja efetuar.
-operacao = int(input("Digite a operacao desejada (1 para soma, 2 para sub, 3 para mult, 4 para div): "))
-if operacao == 1:
-    print("vamos começar as operações de adição")
-    numero_1 = input("Digite o primeiro número: ")
-    numero_2 = input("Digite o segundo número: ")
-    resultado= float(numero_1) + float(numero_2)
+    operacao = int(input("Digite a operacao desejada (1 para soma, 2 para sub, 3 para mult, 4 para div): "))
+    if operacao == 1:
+        print("vamos começar as operações de adição")
+        numero_1 = input("Digite o primeiro número: ")
+        numero_2 = input("Digite o segundo número: ")
+        resultado= float(numero_1) + float(numero_2)
 # cada bloco de if ja tem as operações a serem feita. 
-elif operacao == 2:
-    print("vamos começar as operações de subtração")
-    numero_1 = input("Digite o primeiro número: ")
-    numero_2 = input("Digite o segundo número: ")
-    resultado= float(numero_1) - float(numero_2)
-elif operacao == 3: 
-    print("vamos começar as operações de multiplicação")
-    numero_1 = input("Digite o primeiro número: ")
-    numero_2 = input("Digite o segundo número: ")
-    resultado= float(numero_1) * float(numero_2)
-elif operacao == 4:
-    print("vamos começar as operações de dividir")
-    numero_1 = input("Digite o primeiro número: ")
-    numero_2 = input("Digite o segundo número: ")
-    resultado= float(numero_1) / float(numero_2)
-else: 
-    print("opção invalida")
+    elif operacao == 2:
+        print("vamos começar as operações de subtração")
+        numero_1 = input("Digite o primeiro número: ")
+        numero_2 = input("Digite o segundo número: ")
+        resultado= float(numero_1) - float(numero_2)
+    elif operacao == 3: 
+        print("vamos começar as operações de multiplicação")
+        numero_1 = input("Digite o primeiro número: ")
+        numero_2 = input("Digite o segundo número: ")
+        resultado= float(numero_1) * float(numero_2)
+    elif operacao == 4:
+        print("vamos começar as operações de dividir")
+        numero_1 = input("Digite o primeiro número: ")
+        numero_2 = input("Digite o segundo número: ")
+        resultado= float(numero_1) / float(numero_2)
+    else: 
+        print("opção invalida")
 
-print("O resultado da operação é: " + str(resultado))
+    print("O resultado da operação é: " + str(resultado))
 
-print("*******************************************************************")
-
-
-    
+    operacao = input("Digite [s] para sair o [c] para continuar: ")
